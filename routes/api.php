77e6e4e4bd66411391e0fc7482b8da0f79700e2c
Olawale Olayinka <olayinkaolawale52@gmail.com>
2026-07-17 02:54:57 +0100

    introduce api versioning

--- routes/api.php
+++ routes/api.php
@@ -17,46 +17,47 @@ use Illuminate\Foundation\Auth\EmailVerificationRequest;
 //     'auth:sanctum',
 //     'signed'
 // ])->name('verification.verify');
+Route::prefix('v1')->group(function () {
+    Route::get(
+        '/email/verify/{id}/{hash}',
+        [App\Http\Controllers\API\EmailVerificationController::class, 'verify']
+    )
+    ->middleware(['signed'])
+    ->name('verification.verify');
 
-Route::get(
-    '/email/verify/{id}/{hash}',
-    [App\Http\Controllers\API\EmailVerificationController::class, 'verify']
-)
-->middleware(['signed'])
-->name('verification.verify');
+    Route::post('/email/resend', [App\Http\Controllers\API\RegistrationController::class, 'resendVerification']);
 
-Route::post('/email/resend', [App\Http\Controllers\API\RegistrationController::class, 'resendVerification']);
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
+    Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
+    Route::get('/auth/google', [App\Http\Controllers\GoogleController::class, 'googleloginpage']);
+    Route::get('/auth/google/callback', [App\Http\Controllers\GoogleController::class, 'googleLoginCallback']);
+    Route::post('/register',[App\Http\Controllers\API\RegistrationController::class, 'register']);
+    Route::post( '/forgot-password',[App\Http\Controllers\API\AuthController::class,'forgotPassword']);
+    Route::post( '/reset-password',[App\Http\Controllers\API\AuthController::class,'resetPassword']);
+    Route::post('/user/{user}/events/register',[App\Http\Controllers\EventController::class, 'register']);
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::prefix('events')->group(function () {
+            Route::post('/',[App\Http\Controllers\EventController::class, 'create'])->middleware('permission:create-event'); //use gate to ensure that only organizers and admins can create events
+            Route::get('/', [App\Http\Controllers\EventController::class, 'getAllEvents']);
+            Route::patch('/{event}', [App\Http\Controllers\EventController::class, 'update']);
+            Route::delete('/{event}', [App\Http\Controllers\EventController::class, 'delete']);
+            Route::get('/{event}', [App\Http\Controllers\EventController::class, 'show']);
+        });
+        Route::post('logout',[App\Http\Controllers\API\AuthController::class,'logout']);
+        Route::get('/profile', function (Request $request) {
+            return $request->user()->profile;
+        });
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
-Route::post('/login', [App\Http\Controllers\API\AuthController::class, 'login']);
-Route::get('/auth/google', [App\Http\Controllers\GoogleController::class, 'googleloginpage']);
-Route::get('/auth/google/callback', [App\Http\Controllers\GoogleController::class, 'googleLoginCallback']);
-Route::post('/register',[App\Http\Controllers\API\RegistrationController::class, 'register']);
-Route::post( '/forgot-password',[App\Http\Controllers\API\AuthController::class,'forgotPassword']);
-Route::post( '/reset-password',[App\Http\Controllers\API\AuthController::class,'resetPassword']);
-Route::post('/user/{user}/events/register',[App\Http\Controllers\EventController::class, 'register']);
-Route::middleware('auth:sanctum')->group(function () {
-    Route::prefix('events')->group(function () {
-        Route::post('/',[App\Http\Controllers\EventController::class, 'create'])->middleware('permission:create-event'); //use gate to ensure that only organizers and admins can create events
-        Route::get('/', [App\Http\Controllers\EventController::class, 'getAllEvents']);
-        Route::patch('/{event}', [App\Http\Controllers\EventController::class, 'update']);
-        Route::delete('/{event}', [App\Http\Controllers\EventController::class, 'delete']);
-        Route::get('/{event}', [App\Http\Controllers\EventController::class, 'show']);
+        Route::post('/profile', function (Request $request) {
+            // Update user profile logic
+        });
     });
-    Route::post('logout',[App\Http\Controllers\API\AuthController::class,'logout']);
-    Route::get('/profile', function (Request $request) {
-        return $request->user()->profile;
+
+    Route::middleware(['auth:sanctum', 'admin'])->group(function () {
+
     });
-
-    Route::post('/profile', function (Request $request) {
-        // Update user profile logic
-    });
-});
-
-Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-
 });
 
 // Route::middleware('auth:sanctum')->get('/sanctum-test', function () {
