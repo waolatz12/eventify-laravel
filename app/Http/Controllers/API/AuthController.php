17716a1970de4cc5f1a638b3987951d71627c734
Olawale Olayinka <olayinkaolawale52@gmail.com>
2026-07-22 14:56:35 +0100

    debugging login endpoint in production

--- app/Http/Controllers/API/AuthController.php
+++ app/Http/Controllers/API/AuthController.php
@@ -58,6 +58,7 @@ class AuthController extends Controller
                     $request->validated()
                 );
 
+            dd($response);
             return response()->json([
                 'status' => 'success',
                 'message' => 'Login Successful',
