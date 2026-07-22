1c75d7be07d902fc28f0bcde37fe7261975b9e85
Olawale Olayinka <olayinkaolawale52@gmail.com>
2026-07-22 14:10:08 +0100

    testing connection login

--- app/Enums/UserRole.php
+++ app/Enums/UserRole.php
@@ -4,7 +4,7 @@ namespace App\Enums;
 
 enum UserRole: string
 {
-    case ADMIN = 'admin';
+    case ADMIN = 'admin' || 'Admin';
     case ORGANIZER = 'organizer';
     case ATTENDEE = 'attendee';
     case VENDOR = 'vendor';
